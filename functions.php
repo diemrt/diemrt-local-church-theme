5c8ff2baa2bbba883af85b810439583031ee4831
diegomartignoni <156002111+diegomartignoni@users.noreply.github.com>
2024-03-05 18:15:48 +0100

    Add code documentation

--- functions.php
+++ functions.php
@@ -1,8 +1,13 @@
 <?php
+
+/**
+ * Custom function to load my persdnal style sheets.
+ */
 function localchurch_register_styles() {
-    wp_enqueue_style('localchurch-style', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
+    wp_enqueue_style('localchurch-style', get_template_directory_uri() . "/style.css", array('localchurch-style'), '1.0', 'all');
 }
 
+//It loads my custom function inside the main script.
 add_action('wp_enqueue_scripts', 'localchurch_register_styles');
 
 ?>
