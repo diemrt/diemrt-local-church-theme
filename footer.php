7722397b2c47e4d6bcf5ce6e64e2927b5c05d09a
Diego Martignoni <diego.martignoni@abenergie.it>
2024-03-08 02:28:24 +0100

    Fix footer

--- footer.php
+++ footer.php
@@ -1,25 +1,23 @@
 <footer class="footer-section">
     <div class="container in-footer">
         <div id="w-node-_2a638440-9fff-2e4a-67aa-fa569417e69d-9417e69b">
-            <h6>Sunday Church</h6>
+            <h6><?php bloginfo('name') ?></h6>
         </div>
+        <?php
+        wp_nav_menu(
+            array(
+                'menu' => 'footer-menu',
+                'container' => '',
+                'theme_location' => 'footer-menu',
+                'items_wrap' => '<div id="archive-footer-menu" class="footer-column"><h5>Menu</h5>%3$s</div>'
+            )
+        );
+        ?>
         <div class="footer-column">
-            <h5>Menu</h5><a href="/" aria-current="page" class="footer-link w--current">Home</a><a href="/community" class="footer-link">Our Community</a><a href="/ministries" class="footer-link">Our Ministries</a><a href="/team" class="footer-link">Our Team</a><a href="/blog" class="footer-link">Our Blog</a><a href="/we-believe" class="footer-link">We believe</a><a href="/contact" class="footer-link">Contact
-                Us</a>
-        </div>
-        <div class="footer-column">
-            <h5>CMS Pages</h5><a href="https://sunday-church.webflow.io/events/distinctio-illum" class="footer-link">Event Page</a><a href="https://sunday-church.webflow.io/team/andrew-coleman" class="footer-link">Member Page</a><a href="https://sunday-church.webflow.io/post/7-ways-to-improve-website-usability-and-accessibility" class="footer-link">Blog Post Page</a>
-        </div>
-        <div class="footer-column">
-            <h5>Socials</h5><a href="http://instagram.com" class="footer-link">Instagram</a><a href="http://facebook.com" class="footer-link">Facebook</a>
-        </div>
-        <div class="footer-column">
-            <h5>Service Pages</h5><a href="/template-info/style-guide" class="footer-link">Style Guide</a><a href="/template-info/changelog" class="footer-link">Changelog</a><a href="/template-info/licenses" class="footer-link">Licensing</a>
+            <h5>Socials</h5><li><a href="http://instagram.com" class="footer-link">Instagram</a></li><li><a href="http://facebook.com" class="footer-link">Facebook</a></li>
         </div>
         <div id="w-node-_2a638440-9fff-2e4a-67aa-fa569417e6bc-9417e69b" class="footer-copyright">
-            <div>Powered by <a href="https://webflow.com/" target="_blank" class="copyright-link">Webflow</a></div>
-            <div>Created by <a href="https://khimanin.webflow.io" target="_blank" class="copyright-link">Webflow
-                    Minimal Templates </a></div>
+            © <?php echo date("Y") ?> <?php bloginfo('name') ?>
         </div>
     </div>
 </footer>
--- page.php
+++ page.php
@@ -11,10 +11,16 @@ get_header();
                     <div data-w-id="0cdfc0c9-e628-8baf-6bea-330630344382" class="menu-icon-line"></div>
                     <div data-w-id="0cdfc0c9-e628-8baf-6bea-330630344381" class="menu-icon-line second"></div>
                 </div>
-                <nav class="menu-content hidden" style="display: none; opacity: 0;"><a href="/" class="menu-link">Home</a><a href="/contact" class="menu-link">Contact Us</a>
-                    <div class="menu-divider"></div><a href="/community" class="menu-link">Our Community</a><a href="/ministries" aria-current="page" class="menu-link w--current">Our Ministries</a><a href="/team" class="menu-link">Our Team</a><a href="/blog" class="menu-link">Our Blog</a>
-                    <div class="menu-divider"></div><a href="/we-believe" class="menu-link">We Believe</a>
-                </nav>
+                <?php
+                wp_nav_menu(
+                    array(
+                        'menu' => 'header-menu',
+                        'container' => '',
+                        'theme_location' => 'header-menu',
+                        'items_wrap' => '<nav id="archive-menu" class="menu-content hidden" style="display: none; opacity: 0;">%3$s</nav>'
+                    )
+                );
+                ?>
             </div>
         </div>
         <?php
