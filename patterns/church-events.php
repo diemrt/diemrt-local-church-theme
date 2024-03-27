671f444010d1f2260abe34cc557f3dabfd9c1d7e
Diego Martignoni <diego.martignoni@abenergie.it>
2024-03-27 16:56:12 +0100

    Disable single event click

--- patterns/church-events.php
+++ patterns/church-events.php
@@ -19,7 +19,7 @@ if ($the_query->have_posts()) {
     while ($the_query->have_posts()) {
         $the_query->the_post();
 ?>
-        <div role="listitem" class="w-dyn-item"><a data-w-id="cd9aefab-bb10-41fa-d31f-89aeda6440fa" href="<?php the_permalink() ?>" class="event-content w-inline-block" style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+        <div role="listitem" class="w-dyn-item"><div data-w-id="cd9aefab-bb10-41fa-d31f-89aeda6440fa" class="event-content w-inline-block" style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
                 <div class="event-info" style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
                     <h6 class="event-item-time"><?php echo get_post_meta(get_the_ID(), 'schedule', true); ?></h6>
                     <h3 style="color: #fff;" class="event-item-heading">
@@ -34,7 +34,7 @@ if ($the_query->have_posts()) {
                         <img width="600" src="<?php echo $image[0]; ?>" alt="Church event image" style="width: auto; height: 100%; max-width: 100%; object-fit: cover;" />
                     </div>
                 <?php endif; ?>
-            </a></div>
+                </div></div>
 <?php
     }
 }
