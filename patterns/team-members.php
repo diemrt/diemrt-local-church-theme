bd2c22370dc11473b7999da930d5c39f117d3118
Diego Martignoni <diego.martignoni@abenergie.it>
2024-03-29 03:19:07 +0100

    Fix team members

--- patterns/team-members.php
+++ patterns/team-members.php
@@ -22,13 +22,13 @@ if ($the_query->have_posts()) {
 
 ?>
         <div role="listitem" class="team-item w-dyn-item">
-            <div class="team-item-link w-inline-block">
+            <a href="<?php the_permalink() ?>" class="team-item-link w-inline-block">
                 <div data-w-id="e9b171a0-269d-9118-f596-afe5607acc5a" class="animation-image-wrap"><img alt="" src="<?php echo $image[0]; ?>" class="animation-image"></div>
                 <div class="team-item-headings">
                     <h5 class="no-margin"><?php the_title() ?></h5>
                     <h6 class="no-margin text-blue"><?php echo get_post_meta(get_the_ID(), 'role', true); ?></h6>
                 </div>
-            </div>
+            </a>
         </div>
     <?php
     }
